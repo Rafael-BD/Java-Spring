ab8a104965553181ddfd9c28e7a834d4a6d48cc7
Rafael Bettini Dias <rafael8885326@gmail.com>
2022-09-26 21:46:33 +0000

    hibernate

--- DDL.sql
+++ DDL.sql
@@ -38,8 +38,8 @@ create table ant_anotacao (
 );
 
 insert into usr_usuario (usr_nome, usr_senha)
-    values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
-insert into aut_autorizacao (aut_nome)
+    values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/');
+insert into aut_autorizacao (aut_nome)gMZhdc3e7LIovAklqM6C
     values ('ROLE_ADMIN');
 insert into uau_usuario_autorizacao values (1, 1);
 insert into ant_anotacao(ant_texto, usr_id) 
